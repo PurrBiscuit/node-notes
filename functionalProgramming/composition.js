35b9670c26f15d3c09de49482683e9a375c6bdd4
Purrbiscuit <spurr@articulate.com>
2021-03-29 17:46:36 -0400

    update functionalProgramming/composition.js examples

--- functionalProgramming/composition.js
+++ functionalProgramming/composition.js
@@ -1,7 +1,7 @@
 const { underlinePrint } = require('../utils')
 
 // function composition is using the output of one function
-// as the input to another function (and so one with many
+// as the input to another function (and so on with many
 // other functions chained together if needed)
 
 // there are some libraries in Ramda that can help
@@ -13,7 +13,7 @@ const { underlinePrint } = require('../utils')
 // vanilla javascript to create a bunch of small function
 // and then compose them together
 
-const trim = str => str.trim()
+const trimString = str => str.trim()
 
 const wrapInDiv = str => `<div>${str}</div>`
 
@@ -22,8 +22,27 @@ const toLowerCase = str => str.toLowerCase()
 // compose all the small functions together to get the result
 // after calling one inside the other, etc.
 
-const result = wrapInDiv(toLowerCase(trim('This is the string')))
+const result = wrapInDiv(toLowerCase(trimString('This is the string')))
 
 underlinePrint('Result from functional composition example using vanilla Javascript:')
 
 console.log(`Result is -> ${result}`)
+
+// example of the same function composition, but
+// this time it's being done with ramda functions
+
+const {
+  compose,
+  toLower,
+  trim
+} = require('ramda')
+
+const createDivElement = compose(
+  wrapInDiv,
+  toLower,
+  trim
+)
+
+underlinePrint('Result from functional composition example using ramda:')
+
+console.log(`Result is -> ${createDivElement('This is Another String')}`)
