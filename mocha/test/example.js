7659b6a54afebfbcb365029ad4d8ae69f1b7b674
Purrbiscuit <spurr@articulate.com>
2021-04-13 19:05:41 -0400

    add a couple of before and beforeEach hooks to mocha suite

--- mocha/test/example.js
+++ mocha/test/example.js
@@ -1,7 +1,11 @@
 describe('example test suite', () => {
-  it('should pass', () => {
-    
-  })
+  beforeEach('first beforeEach hook', () => console.log('this is the first beforeEach hook'))
+  beforeEach('second beforeEach hook', () => console.log('this is the second beforeEach hook'))
+  before('first before hook', () => console.log('this before is only run once'))
+
+  it('should pass', () => {})
+
+  it('should pass again', () => {})
 
   // uncomment this test to show how a test can fail
   // if an error is thrown in a test using mocha
