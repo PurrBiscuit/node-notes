10295269df48d320502a5650c8625428ceaa1fa1
Purrbiscuit <spurr@articulate.com>
2021-04-13 20:15:55 -0400

    add a random globalEach to test behavior in test/maths

---
+++ mocha/test/maths/globalBeforeEach.js
@@ -0,0 +1,6 @@
+// uncomment to see this beforeEach getting run before every 'it'
+// in the entire test-suite, even though it's defined in maths/
+
+// beforeEach(() => {
+//   console.log('running before EVERYTHING!!!')
+// })
