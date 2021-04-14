99d19bcab4af0df1a356e485fb4418ac3c90ba34
Purrbiscuit <spurr@articulate.com>
2021-04-14 11:27:05 -0400

    add more nested hook examples to nestedDescribes.js

--- mocha/test/example.js
+++ mocha/test/example.js
@@ -3,7 +3,7 @@ const { blue, yellow } = require('chalk')
 describe('example test suite', () => {
   beforeEach('first beforeEach hook', () => console.log(yellow('      this is the first beforeEach hook')))
   beforeEach('second beforeEach hook', () => console.log(yellow('      this is the second beforeEach hook')))
-  before('first before hook', () => console.log(blue('    this before is only run once')))
+  before('first before hook', () => console.log(blue('      this before is only run once')))
 
   it('should pass', () => {})
 
--- mocha/test/nestedDescribes.js
+++ mocha/test/nestedDescribes.js
@@ -1,16 +1,24 @@
-const { yellow } = require('chalk')
+const { blue, yellow } = require('chalk')
 
-describe('this is a first level of a test-suite', () => {
+describe('this is a first level test suite', () => {
+  before('before in first level', () =>
+    console.log(blue('    run before in first level only'))
+  )
+
   beforeEach('beforeEach in first level', () =>
     console.log(yellow('        run before each test in the describes nested below'))
   )
 
-  describe('this is a second level of a test-suite', () => {
+  describe('this is a second level test suite', () => {
+    before('before in first level', () =>
+      console.log(blue('        run before in single second level only'))
+    )
+
     it('this is test number 1', () => {})
     it('this is test number 2', () => {})
   })
 
-  describe('this is also a second level of a test-suite', () => {
+  describe('this is also a second level test suite', () => {
     it('this is test number 3', () => {})
     it('this is test number 4', () => {})
   })
